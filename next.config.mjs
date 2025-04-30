92bd2881b52f6bb42d2e9538d05db85846ab1735
Jens Karlsson <jk224vx@student.lnu.se>
2025-04-30 21:03:06 +0200

    Created a test file for some of my important functions and also created a api routes to run the tests. All tests are approved.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: process.env.NODE_ENV === 'production' ? '/cashtrack' : '',
+    webpack: (config, { dev, isServer }) => {
+        // During production, ignore test files
+        if (!dev) {
+            // Ignore route.js files in api/tests catalog
+            config.module.rules.push({
+                test: /api\/tests\/.*\.js$/,
+                loader: 'ignore-loader',
+            });
+            
+            // Ignore all files in /tests catalog
+            config.module.rules.push({
+                test: /\/tests\/.*\.js$/,
+                loader: 'ignore-loader',
+            });
+        }
+        
+        return config;
+    }
 };
 
 export default nextConfig;
