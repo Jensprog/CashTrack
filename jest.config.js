723b82b21c092bb62723ef485c8e4527153ca34d
Jens Karlsson <jk224vx@student.lnu.se>
2025-05-13 13:15:16 +0200

    Installed automatic testing through jest and react test library. Created a simple test suite with 2 tests and they pass. Will continue to work with some more automatic tests.

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+const nextJest = require('next/jest');
+
+// Path to my Next.js app
+const createJestConfig = nextJest({
+    dir: './',
+});
+
+// Custom jest config to handle module paths, inline with my jsconfig.json
+const customJestConfig = {
+    testEnvironment: 'jest-environment-jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+    },
+};
+
+// Export a function to let Next.js handle it
+module.exports = createJestConfig(customJestConfig);
---
+++ src/components/categories/CategoryForm.test.jsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import CategoryForm from './CategoryForm';
+
+const mockOnSubmit = jest.fn();
+
+describe('CategoryForm', () => {
+    it ('renders without crashing', () => {
+        render(<CategoryForm onSubmit={mockOnSubmit} />);
+
+        expect(screen.getByText(/Kategorityp/i)).toBeInTheDocument();
+        expect(screen.getByText(/Kategorinamn/i)).toBeInTheDocument();
+    });
+
+    it('displays three category type buttons', () => {
+        render(<CategoryForm onSubmit={mockOnSubmit} />);
+
+        expect(screen.getByText(/Inkomst/i)).toBeInTheDocument();
+        expect(screen.getByText(/Sparande/i)).toBeInTheDocument();
+        expect(screen.getByText(/Utgift/i)).toBeInTheDocument();
+    });
+});
